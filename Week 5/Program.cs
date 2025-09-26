7c55e2131ee75f7416a82c999e663f758254afa3
AlejandraDessiree <alejandradessiree@gmail.com>
2025-09-26 12:39:16 -0400

    Added the logic of the prgoram and finished primary funtions

--- Week 5/Program.cs
+++ Week 5/Program.cs
@@ -1,1 +1,293 @@
-﻿
+﻿// Week 5 - Alejandra Arauz - HCC
+using Operations;
+using static System.Console;
+using static Menus.Menu;
+using static System.Math;
+
+class RuningCalc()
+{
+    public static void Main(string[] args)
+    {
+        Principal();
+    }
+}
+
+namespace Menus
+{
+    class Menu()
+    {
+        public static void Principal()
+        {
+            List<string> UserOptions = new() {"1", "2", "3", "4"};
+            string userchoice;
+            while (true)
+            {
+                WriteLine("=== OPTIONS ===\n1: Operations \n2: 2 \n3: 3 \n4: Exit\n=== ======= ===");
+                userchoice = ReadLine();
+                if (UserOptions.Contains(userchoice) == false)
+                {
+                    WriteLine("=== OPTIONS ===\n1: 1 \n2: 2 \n3: 3 \n4: Exit \n=== ======= ===");
+                    userchoice = ReadLine();
+                }
+                else
+                {
+                    if (userchoice == "1")
+                    {
+                        Menu.FirstChoice();
+                    }
+                    else if (userchoice == "2")
+                    {
+                        Menu.SecondChoice();
+                    }
+                    else if (userchoice == "3")
+                    {
+                        Menu.ThirdOption();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        public static void FirstChoice()
+        {
+            List<string> UserOptions = new() { "1", "2", "3", "4", "5"};
+            string userchoice;
+            WriteLine("=== OPTIONS ===\n1: Addition \n2: Substraction \n3: Division \n4: Multiplicacion \n5: Back \n=== ======= ===");
+            userchoice = ReadLine();
+            if (UserOptions.Contains(userchoice) == false)
+            {
+                WriteLine("=== OPTIONS ===\n1: Addition \n2: Substraction \n3: Division \n4: Multiplicacion \n5: Back \n=== ======= ===");
+                userchoice = ReadLine();
+            }
+            else
+            {
+                if (userchoice == "1")
+                {
+                    OperationsAndLogic.Suma();
+                }
+                else if (userchoice == "2")
+                {
+                    OperationsAndLogic.Resta();
+                }
+                else if (userchoice == "3")
+                {
+                    OperationsAndLogic.Division();
+                }
+                else if (userchoice == "4")
+                {
+                    OperationsAndLogic.Multiplicacion();
+                }
+                else
+                {
+                    Menu.Principal();
+                }
+            }
+        }
+        public static void SecondChoice()
+        {
+            List<string> UserOptions = new() { "1", "2", "3", "4", "5", "6"};
+            string userchoice;
+            WriteLine("=== OPTIONS ===\n1: Addition \n2: Substraction \n3: Division \n4: Multiplicacion \n5: Back \n=== ======= ===");
+            userchoice = ReadLine();
+            if (UserOptions.Contains(userchoice) == false)
+            {
+                WriteLine("=== OPTIONS ===\n1: Power \n2: Square Root \n3: Cosine \n4: Sine \n5: Tangent \n=== ======= ===");
+                userchoice = ReadLine();
+            }
+            else
+            {
+                if (userchoice == "1")
+                {
+                    OperationsAndLogic.Potencia();
+                }
+                else if (userchoice == "2")
+                {
+                    OperationsAndLogic.RaizCuadrada();
+                }
+                else if (userchoice == "3")
+                {
+                    OperationsAndLogic.Coseno();
+                }
+                else if (userchoice == "4")
+                {
+                    OperationsAndLogic.Seno();
+                }
+                else if (userchoice == "5")
+                {
+                    OperationsAndLogic.Tangente();
+                }
+                else
+                {
+                    Menu.Principal();
+                }
+            }
+        }
+        public static void ThirdOption()
+        {
+            List<string> UserOptions = new() { "1", "2", "3", "4", "5"};
+            string userchoice;
+            WriteLine("=== OPTIONS ===\n1: It is possitive? \n2: Are equal? \n3: between 0 to 100? \n4: Results \n5: Back \n=== ======= ===");
+            userchoice = ReadLine();
+            if (UserOptions.Contains(userchoice) == false)
+            {
+                WriteLine("=== OPTIONS ===\n1: It is possitive? \n2: Are equal? \n3: between 0 to 100? \n4: Results \n5: Back \n=== ======= ===");
+                userchoice = ReadLine();
+            }
+            else
+            {
+                if (userchoice == "1")
+                {
+                    OperationsAndLogic.Posittive();
+                }
+                else if (userchoice == "2")
+                {
+                    OperationsAndLogic.Equals();
+                }
+                else if (userchoice == "3")
+                {
+                    OperationsAndLogic.InBetween();
+                }
+                else if (userchoice == "4")
+                {
+                    OperationsAndLogic.Resultados();
+                }
+                else
+                {
+                    Menu.Principal();
+                }
+            }
+        }
+    }
+}
+
+namespace Operations
+{
+    class OperationsAndLogic
+    {
+        public static void Operation()
+        {
+
+        }
+        public static void Suma()
+        {
+            WriteLine("Enter your first number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine("Enter your second number: ");
+            int.TryParse(ReadLine(), out int num2);
+            WriteLine($"=== ======= ===\nYour result is: {num1 * num2}\n=== ======= ===");
+        }
+        public static void Resta()
+        {
+            WriteLine("Enter your first number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine("Enter your second number: ");
+            int.TryParse(ReadLine(), out int num2);
+            WriteLine($"=== ======= ===\nYour result is: {num1 * num2}\n=== ======= ===");
+        }
+        public static void Division()
+        {
+            WriteLine("Enter your first number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine("Enter your second number: ");
+            int.TryParse(ReadLine(), out int num2);
+            WriteLine($"=== ======= ===\nYour result is: {num1 * num2}\n=== ======= ===");
+        }
+        public static void Multiplicacion()
+        {
+            WriteLine("Enter your first number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine("Enter your second number: ");
+            int.TryParse(ReadLine(), out int num2);
+            WriteLine($"=== ======= ===\nYour result is: {num1 * num2}\n=== ======= ===");
+        }
+        public static void Potencia()
+        {
+            WriteLine("Enter your number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine("Enter your power: ");
+            int.TryParse(ReadLine(), out int num2);
+            WriteLine($"=== ======= ===\nYour result is: {Math.Pow(num1, num2)}\n=== ======= ===");
+        }
+        public static void RaizCuadrada()
+        {
+            WriteLine("Enter your number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine($"=== ======= ===\nYour result is: {Math.Sqrt(num1)}\n=== ======= ===");
+        }
+        public static void Coseno()
+        {
+            WriteLine("Enter your number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine($"=== ======= ===\nYour result is: {Math.Cos(num1)}\n=== ======= ===");
+        }
+        public static void Seno()
+        {
+            WriteLine("Enter your number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine($"=== ======= ===\nYour result is: {Math.Sin(num1)}\n=== ======= ===");
+        }
+        public static void Tangente()
+        {
+            WriteLine("Enter your number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine($"=== ======= ===\nYour result is: {Math.Tan(num1)}\n=== ======= ===");
+        }
+        public static void Posittive()
+        {
+            WriteLine("Enter your first number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine("Enter your second number: ");
+            int.TryParse(ReadLine(), out int num2);
+            WriteLine($"=== ======= ===\nIs {num1} positive? {num1 > 0}\n=== ======= ===");
+        }
+        public static void Equals()
+        {
+            WriteLine("Enter your first number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine("Enter your second number: ");
+            int.TryParse(ReadLine(), out int num2);
+            WriteLine($"=== ======= ===\nAre both numbers equal? {num1 == num2}\n=== ======= ===");
+        }
+        public static void InBetween()
+        {
+            WriteLine("Enter your first number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine("Enter your second number: ");
+            int.TryParse(ReadLine(), out int num2);
+            WriteLine($"=== ======= ===\nIs {num1} between 0 and 100? {num1 >= 0 && num1 <= 100}\n=== ======= ===");
+        }
+        public static void Resultados()
+        {
+            WriteLine("Enter your first number: ");
+            int.TryParse(ReadLine(), out int num1);
+            WriteLine("Enter your second number: ");
+            int.TryParse(ReadLine(), out int num2);
+            if (num1 == num2)
+            {
+                WriteLine($"=== ======= ===\nIs Equal? {num1 == num2}");
+            }
+            else
+            {
+                WriteLine($"=== ======= ===\nIs Equal? {num1 != num2}");
+            }
+            if (num1 < num2)
+            {
+                WriteLine($"Is Less Than? {num1 < num2}");
+            }
+            else
+            {
+                WriteLine($"Is Less Than? {num1 < num2}");
+            }
+            if (num1 > num2)
+            {
+                WriteLine($"Is Greater Than? {num1 > num2}");
+            }
+            else
+            {
+                WriteLine($"Is Greater Than? {num1 > num2}");
+            }
+        }
+    }
+}
